9b8eba63d57e8d1c5f8d1bc7cf2457750277b1d3
PolcovnicMiya <aleksandrsgibnev39@gmail.com>
2024-08-07 18:46:17 +0500

    new main file

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+import uvicorn
+from app import router as all_router
+app = FastAPI()
 
-m_app = FastAPI()
- 
+app.include_router(all_router)
 
-@m_app.get("/")
+@app.get("/")
 def standart():
     return{
         "hello":"епт"
     }
+
+if __name__ == "__main__":
+    uvicorn.run(app = "main:app")
