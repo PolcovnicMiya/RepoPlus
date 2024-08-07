3618582176f2f6716c08619e0352207e426ae912
PolcovnicMiya <aleksandrsgibnev39@gmail.com>
2024-08-07 20:08:46 +0500

    add lifespan but not end work with him

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 import uvicorn
 from app import router as all_router
-app = FastAPI()
+from contextlib import asynccontextmanager
+from app.settings.db_connection import db_session
+
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    #start
+    yield
+    #finish
+    await db_session.dispoce()
+
+
+app = FastAPI(
+    lifespan=lifespan,
+)
 
 app.include_router(all_router)
 
