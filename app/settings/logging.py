fbc19aec13d472a44cf072c1244c70bcccba5998
PolcovnicMiya <aleksandrsgibnev39@gmail.com>
2024-08-14 19:48:14 +0500

    add loging and setting for logging

---
+++ app/settings/logging.py
@@ -0,0 +1,7 @@
+import logging
+def log_conf(level = logging.INFO):
+    logging.basicConfig(
+        level = level,
+        datefmt = "%Y-%m-%d %H:%M:%S",
+        format =  "[%(asctime)s.%(msecs)03d] %(module)20s:%(lineno)-3d %(levelname)7s - %(message)s"
+    )
--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import uvicorn
 from app import router as all_router
 from contextlib import asynccontextmanager
 from app.settings.db_connection import db_session
+import logging
+from app.settings.logging import log_conf
 
+log = logging.getLogger("__name__")
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):
+    log_conf()
+    log.info("Успешный запуск")
     #start
     yield
     #finish
+    log.info("приложение выключилось")
     await db_session.dispoce()
 
 
